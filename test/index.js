1c26f05293f8617f347379e20ea64eabd13917d2
Faycal Tirich <fayway@gmail.com>
2015-10-19 16:10:04 +0200

    re activate all tests

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@ require('./config/requireJSForTests');
 require('./config/ractiveForTests');
 
 //Run Tests
-//require('./components/IbanSpec');
+require('./components/IbanSpec');
 require('./components/CompteSwitcherSpec');
-//require('./sandbox/RactiveTest');
-//require('./sandbox/RequireJSTest');
+require('./sandbox/RactiveTest');
+require('./sandbox/RequireJSTest');
 
